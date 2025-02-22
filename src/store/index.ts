ae2903708d335d2e4eb4fc2c0aca17cbbd7775e2
berk145 <dereberk1@gmail.com>
2025-02-22 23:01:19 +0300

    removing comments

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
 import { createStore } from 'vuex'
 import raceModule from './race'
-/* import plugins from './plugins' */
 
 export default createStore({
   modules: {
     raceModule,
   },
-  /*  plugins, */
 })
